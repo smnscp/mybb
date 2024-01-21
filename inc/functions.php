fe3349d9a08cee438f2b43415226c49412466445
dvz <devilshakerz@gmail.com>
2024-01-21 18:47:16 +0100

    Use `CURLOPT_DISALLOW_USERNAME_IN_URL` if supported

--- inc/functions.php
+++ inc/functions.php
@@ -7042,6 +7042,11 @@ function fetch_remote_file($url, $post_data = array(), $max_redirects = 20)
 			);
 		}
 
+		if(defined('CURLOPT_DISALLOW_USERNAME_IN_URL'))
+		{
+			$curlopt[CURLOPT_DISALLOW_USERNAME_IN_URL] = true;
+		}
+
 		if(!empty($post_body))
 		{
 			$curlopt[CURLOPT_POST] = 1;
